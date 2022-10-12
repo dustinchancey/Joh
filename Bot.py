34e624281192e0794669e59005ee9e42234a4b12
dustinchancey <mirikfrog@gmail.com>
2022-10-11 18:25:06 -0700

    Hide TOKEN

--- Bot.py
+++ Bot.py
@@ -1,6 +1,9 @@
 # This example requires the 'message_content' intent.
-
+import os
+from dotenv import load_dotenv
 import discord
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -25,7 +28,16 @@ async def on_message(message):
     if message.content.startswith('hateful'):
         await message.channel.send(r'https://cdn.discordapp.com/attachments/1029544303485268041/1029557278552105000/hqdefault.jpg')
 
-    if message.content.startswith('why'):
-        await message.channel.send('it was a little funny')
+    if message.content.startswith('Jor'):
+        # STUFF BROKEN BELOW LOL
+        # # me = client.get_user('184829294051196928')
+        # # await me.send(r'https://cdn.discordapp.com/attachments/1029544303485268041/1029557278552105000/hqdefault.jpg')  # or whatever you want to send here
+        #
+        # # me = await client.get_user_info('184829294051196928')
+        # # await client.send_message(me, r"https://cdn.discordapp.com/attachments/1029544303485268041/1029557278552105000/hqdefault.jpg!")
+        # user = client.get_user(184829294051196928)
+        # # await user.send('👀')
+        # await client.send_message(user, r"https://cdn.discordapp.com/attachments/1029544303485268041/1029557278552105000/hqdefault.jpg!")
+        await message.author.send(r'https://media.discordapp.net/attachments/935989994735169546/1027423675412123648/36f106124c5091548053978b9fa8a60b.gif')
 
-client.run('MTAyODkwMDc5Njk3ODQzODIwOA.GsQjjE.vBDd0rY-6bGq58cBf-9nN30dRJInE9lip09VX8')
+client.run(TOKEN)
